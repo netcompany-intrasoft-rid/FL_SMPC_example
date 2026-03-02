a95f2dde44e08edcef732311582b59b32789fd92
Petros Dimitrakopoulos <pdimitra@netcompany.com>
2026-03-02 12:41:19 +0200

    removed unused files

--- normal_fl/start_clients_4.sh
+++
@@ -1,5 +0,0 @@
-python client.py --num_clients=4 --client_id=0 &
-python client.py --num_clients=4 --client_id=1 &
-python client.py --num_clients=4 --client_id=2 &
-python client.py --num_clients=4 --client_id=3 &
-wait # Ensures the script waits for all background processes to complete
